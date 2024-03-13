8f9ec4d0b4950e94172b4a53e9ad0922a387bf7e
Jay patel <jayp962001@gmail.com>
2024-03-13 08:55:41 +0530

    Update static.ts

--- src/Experience/static.ts
+++ src/Experience/static.ts
@@ -1,10 +1,10 @@
 const modelPaths = [
-    'https://d2629xvaofl3d3.cloudfront.net/Wall.glb',
     'https://d2629xvaofl3d3.cloudfront.net/Floor.glb',
+    'https://d2629xvaofl3d3.cloudfront.net/Floor_Lamp.glb',
+    'https://d2629xvaofl3d3.cloudfront.net/Wall.glb',
     'https://d2629xvaofl3d3.cloudfront.net/Frame.glb',
     'https://d2629xvaofl3d3.cloudfront.net/Plant.glb',
     'https://d2629xvaofl3d3.cloudfront.net/Coffee_Table.glb',
-    'https://d2629xvaofl3d3.cloudfront.net/Floor_Lamp.glb',
     'https://d2629xvaofl3d3.cloudfront.net/Window.glb',
     'https://d2629xvaofl3d3.cloudfront.net/Sofa.glb',
 ];
