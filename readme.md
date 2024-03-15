ca4f69cde73939c6419eb8c7e893d0f7da965084
Jay patel <jayp962001@gmail.com>
2024-03-15 10:59:23 +0530

    Updated

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Customize3D
+ Showcase Materials
+
+# Installation 
+    1. Clone github repository 
+        git clone https://github.com/rktumuluri63/VC_Customize3D.git
+    2. Install third party packages 
+        npm install 
+    3. Check Everything is working ...
+        npm run dev
--- src/Experience/composer.ts
+++ src/Experience/composer.ts
@@ -80,3 +80,67 @@ export const composer = (renderer: THREE.WebGLRenderer, scene: THREE.Scene, came
 
     return composer;
 };
+
+
+
+
+// import * as THREE from 'three';
+// import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js' ; 
+// import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+// import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass.js';
+// import { OutputPass } from 'three/examples/jsm/postprocessing/OutputPass.js';
+// import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
+// import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+// import { TAARenderPass } from 'three/examples/jsm/postprocessing/TAARenderPass.js';
+
+// export const threeComposer = ( renderer : THREE.WebGLRenderer , scene : THREE.Scene , camera : THREE.PerspectiveCamera ) =>{
+//     let composer = new EffectComposer( renderer );
+//     composer.addPass( new RenderPass( scene, camera ) );
+
+//     const pixelRatio = renderer.getPixelRatio() ; 
+//     let smaaPass = new SMAAPass( window.innerWidth * pixelRatio , window.innerHeight * pixelRatio );
+
+//     let fxaaPass = new ShaderPass( FXAAShader );
+//     fxaaPass.material.uniforms[ 'resolution' ].value.x = 1 / ( window.innerWidth * pixelRatio );
+//     fxaaPass.material.uniforms[ 'resolution' ].value.y = 1 / ( window.innerHeight * pixelRatio );
+//     composer.addPass(fxaaPass) ;
+
+//     let taaRenderPass = new TAARenderPass( scene, camera );
+//     taaRenderPass.unbiased = true;
+//     taaRenderPass.sampleLevel = 3 ; 
+
+//     const elem = document.getElementById('SMAA_C') as HTMLInputElement ; 
+//     elem.addEventListener('input', ()=>{
+//         if( elem.checked ){
+//             composer.addPass( smaaPass );
+//         }
+//         else{
+//             composer.removePass(smaaPass);
+//         }
+//     })
+
+//     const elem2 = document.getElementById('FXAA_C') as HTMLInputElement ; 
+//     elem2.addEventListener('click' , ()=>{
+//         if( elem2.checked ){
+//             composer.addPass(fxaaPass) ; 
+//         }
+//         else{
+//             composer.removePass(fxaaPass) ; 
+//         }
+//     })
+
+//     const elem3 = document.getElementById('TAA_C') as HTMLInputElement ; 
+//     elem3.addEventListener('input' , ()=>{
+//         if( elem3.checked ){
+//             composer.addPass( taaRenderPass );
+//         }
+//         else{
+//             composer.removePass( taaRenderPass );
+//         }
+//     })
+
+//     const outputPass = new OutputPass();
+//     composer.addPass( outputPass );
+
+//     return composer ;
+// }
