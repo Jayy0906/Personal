f7f36722fce2564a3dce8f9ade5aa6933f75961e
Jay patel <jayp962001@gmail.com>
2024-08-09 18:46:55 +0530

    u

--- src/Experience/hdr.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three' ;
-
-export const HDRICtrl = (renderer : THREE.WebGLRenderer) =>{
-    const elem = document.getElementById('intensity-hdri') as HTMLInputElement ;
-    elem.addEventListener('input' , (e)=>{
-        //@ts-ignore
-        renderer.toneMappingExposure = e.target.value ; 
-    }) 
-}
